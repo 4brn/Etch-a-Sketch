70c4ef0e5e6e8565de5d001f1d35a8e7bdca004d
Aleksandar Rangelov <122702818+4brn@users.noreply.github.com>
2024-12-02 17:55:24 +0200

    Premature optimization is the root of all evil!

--- script.js
+++ script.js
@@ -1,74 +1,64 @@
+const canvas = document.querySelector("#canvas");
+const colorPicker = document.querySelector("#color-picker");
+const colorModeButton = document.querySelector("#color");
+const rainbowModeButton = document.querySelector("#rainbow");
+const eraserModeButton = document.querySelector("#eraser");
+const clearButton = document.querySelector("#clear");
+const resolutionSlider = document.querySelector("#resolution");
+const resolutionLabel = document.querySelector("#resolution-label");
+
+// Config
 const WIDTH = 500;
 const HEIGHT = 500;
+
 const DEFAULT_COLOR = "#333333";
 const DEFAULT_RESOLUTION = 16;
 
 let color = DEFAULT_COLOR;
 let resolution = DEFAULT_RESOLUTION;
-
-let colorMode = true;
-let eraserMode = false;
+let mode = "color";
 
 const setColor = (value) => (color = value);
 const setResolution = (value) => (resolution = value);
 
-const canvas = document.querySelector("#canvas");
-const clearButton = document.querySelector("#clear");
-const colorModeButton = document.querySelector("#color-mode");
-const eraserModeButton = document.querySelector("#eraser-mode");
-const colorPicker = document.querySelector("#color");
-const resolutionSlider = document.querySelector("#resolution");
-const resolutionLabel = document.querySelector("#resolution-label");
+const randInt = (max) => Math.floor(Math.random() * max);
 
-function updateResolutionLabel(value) {
-    resolutionLabel.innerHTML = `${value}x${value}`;
-}
+const updateResolution = (newResolution) => {
+    setResolution(newResolution);
+    resolutionSlider.style.accentColor = color;
+    resolutionLabel.innerHTML = `${resolution}x${resolution}`;
+};
 
-function updateResolutionSliderAccentColor(value) {
-    resolutionSlider.style.accentColor = value;
-}
-
-function calculatePixelSize() {
-    return WIDTH / resolution;
-}
-
-function createPixel(size) {
+const createPixel = (size) => {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     Object.assign(pixel.style, {
         width: `${size}px`,
         height: `${size}px`,
-        border: `1px solid ${DEFAULT_COLOR}`,
     });
 
     return pixel;
-}
-
-const applyColor = (e) => {
-    const elementOnCursor = document.elementFromPoint(e.clientX, e.clientY);
-    if (elementOnCursor?.classList.contains("pixel")) {
-        if (colorMode) {
-            elementOnCursor.classList.add("colored");
-            elementOnCursor.style.backgroundColor = color;
-        } else if (eraserMode) {
-            clearPixel(elementOnCursor);
-        }
-    }
 };
 
-function clearPixel(pixel) {
+const clearPixel = (pixel) => {
+    pixel.style.backgroundColor = "";
     pixel.classList.remove("colored");
-}
+};
 
-function setupCanvas(size) {
-    const pixelSize = calculatePixelSize();
+function setupCanvas() {
+    const pixelSize = WIDTH / resolution;
     let mouseDown = false;
+
+    // Rendering the canvas
     // Rows
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < resolution; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
         // Columns
-        for (let j = 0; j < size; j++) {
-            canvas.appendChild(createPixel(pixelSize));
+        for (let j = 0; j < resolution; j++) {
+            row.appendChild(createPixel(pixelSize));
         }
+        canvas.appendChild(row);
     }
 
     canvas.addEventListener("mousedown", (e) => {
@@ -85,41 +75,91 @@ function setupCanvas(size) {
     });
 }
 
-function clearCanvas() {
+const clearCanvas = () =>
     canvas.querySelectorAll(".colored").forEach((pixel) => {
         clearPixel(pixel);
     });
-}
 
-function setupInstruments() {
+const applyColor = (e) => {
+    const pixel = e.target;
+    if (!pixel.classList.contains("pixel")) return;
+
+    switch (mode) {
+        case "color":
+            pixel.style.backgroundColor = color;
+            pixel.classList.add("colored");
+            break;
+        case "rainbow":
+            const r = randInt(256);
+            const g = randInt(256);
+            const b = randInt(256);
+            pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            pixel.classList.add("colored");
+            break;
+        case "eraser":
+            clearPixel(pixel);
+            break;
+        default:
+            console.error("Something's wrong");
+    }
+};
+
+const updateActiveInstrument = (mode) => {
+    switch (mode) {
+        case "color":
+            colorModeButton.classList.add("active");
+            eraserModeButton.classList.remove("active");
+            rainbowModeButton.classList.remove("active");
+            break;
+        case "eraser":
+            colorModeButton.classList.remove("active");
+            eraserModeButton.classList.add("active");
+            rainbowModeButton.classList.remove("active");
+            break;
+        case "rainbow":
+            colorModeButton.classList.remove("active");
+            eraserModeButton.classList.remove("active");
+            rainbowModeButton.classList.add("active");
+            break;
+    }
+};
+
+const setupInstruments = () => {
     setColor(colorPicker.value);
-    setResolution(resolutionSlider.value);
-    updateResolutionLabel(resolutionSlider.value);
-    updateResolutionSliderAccentColor(colorPicker.value);
+    updateResolution(resolutionSlider.value);
 
     colorPicker.oninput = (e) => {
         setColor(e.target.value);
-        updateResolutionSliderAccentColor(e.target.value);
+        resolutionSlider.style.accentColor = e.target.value;
     };
 
     colorModeButton.onclick = () => {
-        colorMode = true;
-        eraserMode = false;
+        mode = "color";
+        updateActiveInstrument(mode);
+    };
+
+    rainbowModeButton.onclick = () => {
+        mode = "rainbow";
+        updateActiveInstrument(mode);
     };
 
     eraserModeButton.onclick = () => {
-        eraserMode = true;
-        colorMode = false;
+        mode = "eraser";
+        updateActiveInstrument(mode);
     };
 
-    clearButton.onclick = () => clearCanvas();
+    clearButton.onclick = () => {
+        clearCanvas();
+    };
+
     resolutionSlider.onchange = (e) => {
-        setResolution(e.target.value);
+        updateResolution(e.target.value);
         canvas.replaceChildren();
         setupCanvas(resolution);
     };
-    resolutionSlider.onmousemove = (e) => updateResolutionLabel(e.target.value);
-}
+
+    resolutionSlider.onmousemove = (e) => updateResolution(e.target.value);
+};
 
 window.onload = () => {
     setupInstruments();
